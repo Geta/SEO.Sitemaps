35ff8b41a5bf7007349bad05bfa9b93da1aaff85
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2015-10-06 19:11:11 +0300

    version updated to 1.5.3

--- Geta.SEO.Sitemaps/Properties/AssemblyInfo.cs
+++ Geta.SEO.Sitemaps/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9f3a4ec0-97a5-47d5-91b2-3e60843d0ff1")]
-[assembly: AssemblyVersion("1.5.2.0")]
-[assembly: AssemblyInformationalVersion("1.5.2")]
+[assembly: AssemblyVersion("1.5.3.0")]
+[assembly: AssemblyInformationalVersion("1.5.3")]
 [assembly: InternalsVisibleTo("Geta.SEO.Sitemaps.Tests")]
