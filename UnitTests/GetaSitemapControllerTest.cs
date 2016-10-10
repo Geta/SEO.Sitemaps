07cbd9013a954fa01866ec48a49484b33e452f9e
Carl-Erik Kopseng <carlerik@gmail.com>
2016-10-10 19:10:44 +0200

    Add failing test for Gzip in Accept-Encoding

--- UnitTests/GetaSitemapControllerTest.cs
+++ UnitTests/GetaSitemapControllerTest.cs
@@ -19,58 +19,143 @@ namespace Tests
         SitemapXmlGeneratorFactory factory = Substitute.For<SitemapXmlGeneratorFactory>();
         ISitemapXmlGenerator sitemapXmlGenerator = Substitute.For<ISitemapXmlGenerator>();
 
-        public GetaSitemapController createController(ISitemapRepository repo, SitemapXmlGeneratorFactory factory)
-        {
-            var controller = new GetaSitemapController(repo,factory);
-            controller.ControllerContext = createControllerContext();
-
-            return controller;
-        }
 
         [Fact]
         public void ReturnsHttpNotFoundResultWhenMissingSitemap()
         {
+            // Arrange
             var controller = createController(repo, factory);
 
-            Assert.IsType<HttpNotFoundResult>(controller.Index());
+            // Act
+            var actionResult = controller.Index();
+
+            // Assert
+            Assert.IsType<HttpNotFoundResult>(actionResult);
         }
 
         [Fact]
         public void ReturnsSitemapWhenRepoIsNonEmpty()
         {
+            // Arrange
             var controller = createController(repo, factory);
-            controller.Response.Filter = new MemoryStream();
+            addDummySitemapData(repo);
 
-            var sitemapData = new SitemapData();
-            sitemapData.Data = new byte[] {0, 1, 2, 3, 4};
-            
-            repo.GetSitemapData(Arg.Any<string>()).Returns(sitemapData);
+            // Act
+            var actionResult = controller.Index();
 
-            Assert.IsType<FileContentResult>(controller.Index());
+            // Assert
+            Assert.IsType<FileContentResult>(actionResult);
+        }
+
+
+        [Fact]
+        public void ChecksAcceptHeaderBeforeSettingGzipEncoding()
+        {
+            // Arrange
+            var controller = createController(repo, factory);
+            addDummySitemapData(repo);
+
+            // Act
+            ActionResult result = controller.Index();
+
+            // Assert
+            var encoding = controller.Response.Headers.Get("Content-Encoding");
+            Assert.NotEqual("gzip", encoding);
+        }
+
+        [Fact]
+        public void AddsGzipEncodingWhenAccepted()
+        {
+
+            // Arrange
+            var httpRequestBase = createRequestBase();
+            httpRequestBase.Headers.Add("Accept-Encoding", "gzip, deflate, br");
+            var requestContext = createRequestContext(httpRequestBase, createResponseBase());
+
+            var controller = createController(repo, factory);
+            addDummySitemapData(repo);
+
+            // Act
+            ActionResult result = controller.Index();
+
+            // Assert
+            var encoding = controller.Response.Headers.Get("Content-Encoding");
+            Assert.Equal("gzip", encoding);
         }
 
         private static ControllerContext createControllerContext()
         {
-            Uri dummyUri = new Uri("http://foo.bar");
+            var requestContext = createRequestContext(createRequestBase(), createResponseBase());
 
+            return createControllerContext(requestContext);
+        }
+
+        private static ControllerContext createControllerContext(RequestContext requestContext)
+        {
             var context = new ControllerContext();
+            context.RequestContext = requestContext;
 
-            var requestBase = Substitute.For<HttpRequestBase>();
-            requestBase.Url.Returns(dummyUri);
-            requestBase.ServerVariables.Returns(new System.Collections.Specialized.NameValueCollection());
+            return context;
+        }
 
-            var responseBase = Substitute.For<HttpResponseBase>();
-            responseBase.Headers.Returns(new System.Collections.Specialized.NameValueCollection());
-
-            var httpContext = Substitute.For<HttpContextBase>();
-            httpContext.Request.Returns(requestBase);
-            httpContext.Response.Returns(responseBase);
+        private static RequestContext createRequestContext(HttpRequestBase requestBase, HttpResponseBase responseBase)
+        {
+            var httpContext = createHttpContext(requestBase, responseBase);
 
             var requestContext = new RequestContext();
             requestContext.HttpContext = httpContext;
+            return requestContext;
+        }
 
-            context.RequestContext = requestContext;
-            return context;
+        private static HttpContextBase createHttpContext(HttpRequestBase requestBase, HttpResponseBase responseBase)
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            httpContext.Request.Returns(requestBase);
+            httpContext.Response.Returns(responseBase);
+            return httpContext;
+        }
+
+        private static HttpResponseBase createResponseBase()
+        {
+            var responseBase = Substitute.For<HttpResponseBase>();
+            var collection = new System.Collections.Specialized.NameValueCollection();
+            responseBase.Headers.Returns(collection);
+            responseBase.When(x => x.AppendHeader(Arg.Any<string>(), Arg.Any<string>()))
+                .Do(args => collection.Add((string) args[0], (string) args[1]));
+
+            return responseBase;
+        }
+
+        private static HttpRequestBase createRequestBase()
+        {
+            Uri dummyUri = new Uri("http://foo.bar");
+            var requestBase = Substitute.For<HttpRequestBase>();
+            requestBase.Url.Returns(dummyUri);
+            requestBase.Headers.Returns(new System.Collections.Specialized.NameValueCollection());
+
+            return requestBase;
+        }
+
+        private static void addDummySitemapData(ISitemapRepository repo2)
+        {
+            var sitemapData = new SitemapData();
+            sitemapData.Data = new byte[] { 0, 1, 2, 3, 4 };
+            repo2.GetSitemapData(Arg.Any<string>()).Returns(sitemapData);
+        }
+
+        public static GetaSitemapController createController(ISitemapRepository repo, SitemapXmlGeneratorFactory factory)
+        {
+            return createController(repo, factory, createControllerContext());
+        }
+
+        private static GetaSitemapController createController(ISitemapRepository repo, SitemapXmlGeneratorFactory factory,
+            ControllerContext controllerContext)
+        {
+            var controller = new GetaSitemapController(repo, factory);
+            controller.ControllerContext = controllerContext;
+            controller.Response.Filter = new MemoryStream();
+
+            return controller;
         }
     }
 }
