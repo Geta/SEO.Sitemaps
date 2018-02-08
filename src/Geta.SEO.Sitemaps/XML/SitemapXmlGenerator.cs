00c48ebef6c0dd8eebd24275d5657b84fbc2ac63
Kenneth Krohn <kenneth.krohn@creuna.fi>
2018-02-08 15:58:23 +0200

    Use InvariantCulture when writing lastmod to avoid current culture causing invalid sitemap datetime formatting regardless of DateTimeFormat constant value, e.g. running on server with Finnish culture wouldformat time with "." as separator

--- src/Geta.SEO.Sitemaps/XML/SitemapXmlGenerator.cs
+++ src/Geta.SEO.Sitemaps/XML/SitemapXmlGenerator.cs
@@ -328,7 +328,7 @@ namespace Geta.SEO.Sitemaps.XML
             var element = new XElement(
                 SitemapXmlNamespace + "url",
                 new XElement(SitemapXmlNamespace + "loc", url),
-                new XElement(SitemapXmlNamespace + "lastmod", modified.ToString(DateTimeFormat)),
+                new XElement(SitemapXmlNamespace + "lastmod", modified.ToString(DateTimeFormat, CultureInfo.InvariantCulture)),
                 new XElement(SitemapXmlNamespace + "changefreq", (property != null) ? property.ChangeFreq : "weekly"),
                 new XElement(SitemapXmlNamespace + "priority", (property != null) ? property.Priority : GetPriority(url))
             );
